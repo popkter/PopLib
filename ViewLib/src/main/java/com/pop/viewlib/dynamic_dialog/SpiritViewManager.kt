816f4064864b4cb580394006e128718c9b83497b
POP <iKotliner@outlook.com>
2024-06-19 11:29:47 +0800

    feat: fix publish

--- ViewLib/src/main/java/com/pop/viewlib/dynamic_dialog/SpiritViewManager.kt
+++ ViewLib/src/main/java/com/pop/viewlib/dynamic_dialog/SpiritViewManager.kt
@@ -41,7 +41,6 @@ import kotlin.math.sqrt
  * [context] Context For get WindowManager & create rootView
  */
 
-@Deprecated("instead of [QuickFloatViewManager] ")
 @SuppressLint("ClickableViewAccessibility")
 @RequiresApi(Build.VERSION_CODES.O)
 class SpiritViewManager(private val context: Context) : View.OnTouchListener,
